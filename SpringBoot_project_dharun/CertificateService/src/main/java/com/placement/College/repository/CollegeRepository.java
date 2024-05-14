d0d0b9b0871c4883483e28aacdfe33c02c0be9d9
mr-weaker <140969580+mr-weaker@users.noreply.github.com>
2024-05-15 00:44:01 +0530

    Delete SpringBoot_project_dharun/CertificateService directory

--- SpringBoot_project_dharun/CertificateService/src/main/java/com/placement/College/repository/CollegeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.placement.College.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.placement.College.entity.College;
-
-public interface CollegeRepository extends JpaRepository<College, Long>{
-
-}
